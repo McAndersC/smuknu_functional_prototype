951850966f3e7c7fb30422ff46785bb7c93d6a96
Anders Christensen <anders@medieskolerne.dk>
2023-08-16 13:10:20 +0200

    Added Icons

--- src/app/layout/page.js
+++ src/app/layout/page.js
@@ -1,5 +1,16 @@
+import { quickFont } from '@/utils/fonts';
+import styles from './page.module.css';
+import { FaFacebook, FaInstagram } from 'react-icons/fa';
+
 export default function Layout() {
 
-    return <div>Layout</div>
+    return <div>
+        
+        <h1 className={`${quickFont.className} ${styles.headline} ${styles.white}`}>MEDLEM?</h1>
+        <div>Standard Font</div>
+        <FaFacebook className={styles.icon}/>
+        <FaInstagram className={styles.icon} />
+        
+    </div>
 
 }
---
+++ src/utils/fonts.js
@@ -0,0 +1,7 @@
+import { Quicksand } from 'next/font/google'
+
+export const quickFont = Quicksand({
+    weight: ['300', '400', '500', '600', '700'],
+    subsets: ['latin'],
+    display: 'swap',
+});
