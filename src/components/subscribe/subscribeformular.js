1f4951fd18223a6fe28fe0546b39c7e0ec4c1bf2
Anders Christensen <anders@medieskolerne.dk>
2023-08-21 13:41:04 +0200

    Updated Form

--- src/components/subscribe/subscribeformular.js
+++ src/components/subscribe/subscribeformular.js
@@ -1,24 +1,53 @@
-
+"use client"
 import styles from './subscriberformular.module.css';
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 const SubscribeForm = () => {
 
+    const [subscriberName, setSubscriberName] = useState("");
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+      } = useForm();
+
+      const onSubmit = (data) => {
+        console.log(data);
+
+        fetch(`https://smuknu.webmcdm.dk/subscribe`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "name" : data.name,
+                "email" : data.email,
+                "message": data.message
+            }),
+        }).then(res => res.json()).then(data => setSubscriberName(data.name));
+            
+
+      }; // your form submit function which will invoke after successful validation
+
+      
+      
     return (
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
             <fieldset>
-                <legend>Subscribers</legend>
+                <legend>Subscribers {subscriberName}</legend>
 
                 <label>Navn
-                    <input />
+                    <input {...register("name")}/>
                 </label>
 
                 <label>Email:
-                    <input />
+                    <input {...register("email")}/>
                 </label>
 
-                
                 <label>Message:
-                    <textarea />
+                    <textarea {...register("message")}/>
                 </label>
 
                 <button>Send</button>
